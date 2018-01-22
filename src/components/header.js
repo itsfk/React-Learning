c967c8bed1e2b986d50661a3c19715f1c6a60dba
Faiz <faiz.ggmu@gmail.com>
2018-01-22 22:11:13 +0500

    Initial Commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+class Header extends React.Component{
+   render(){
+       return(
+        <header>
+        <div>Logo</div>
+        <input/>
+        </header>
+       )
+   }
+}
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/header'
+
+const App =()=>{
+    return( 
+      <div>
+      <Header/>
+      <h1>Hello World</h1>  
+      
+      </div>
+    
+    )
+}
+ReactDOM.render(<App/>,document.querySelector('#root'));
+
